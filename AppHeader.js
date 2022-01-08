39ae555259b13dc2b824fd32138e601ddfbba76c
vjo001 <89363371+vjo001@users.noreply.github.com>
2022-01-07 19:49:28 -0500

    Add files via upload

---
+++ AppHeader.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class AppHeader extends React.Component{
+  render(){
+    return(
+      <View style= {styles.textContainer}>
+        <Text style={styles.text}>🎧 DJ WhiteHat 🎧 </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  textContainer:{
+    backgroundColor: 'blue',
+    height:70,
+    
+  },
+  text:{
+    color: 'white',
+    padding: 20,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
